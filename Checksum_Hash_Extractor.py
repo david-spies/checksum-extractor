cda5486186956301be408e48057d785bbffc8723
David Spies <53583199+david-spies@users.noreply.github.com>
2019-08-05 18:51:01 -0700

    Updated files
    Updated files include new functionality to read .msi files and retrieve MD5 hash

--- Checksum_Hash_Extractor.py
+++ Checksum_Hash_Extractor.py
@@ -6,7 +6,8 @@ import hashlib
 
 def open_file():
     result = filedialog.askopenfilename(initialdir="/", title="select file", filetypes=(
-    ("text files", ".txt"), ("python files", ".py"), ("iso files", ".iso"), ("exe files", ".exe")))
+    ("text files", ".txt"), ("python files", ".py"), ("iso files", ".iso"), ("exe files", ".exe"),
+    ("msi files", ".msi")))
     hasher = hashlib.sha256()
     with open(result, 'rb') as check_sum:
         content = check_sum.read()
@@ -26,7 +27,8 @@ def open_file():
 
 def get_file():
     result = filedialog.askopenfilename(initialdir="/", title="select file", filetypes=(
-    ("text files", ".txt"), ("python files", ".py"), ("iso files", ".iso"), ("exe files", ".exe")))
+    ("text files", ".txt"), ("python files", ".py"), ("iso files", ".iso"), ("exe files", ".exe"),
+    ("msi files", ".msi")))
     hasher = hashlib.sha512()
     with open(result, 'rb') as check_sum:
         content = check_sum.read()
@@ -44,16 +46,44 @@ def get_file():
     f.close()
 
 
+def fetch_file():
+    result = filedialog.askopenfilename(initialdir="/", title="select file", filetypes=(
+    ("text files", ".txt"), ("python files", ".py"), ("iso files", ".iso"), ("exe files", ".exe"),
+    ("msi files", ".msi")))
+    hasher = hashlib.md5()
+    with open(result, 'rb') as check_sum:
+        content = check_sum.read()
+        hasher.update(content)
+    print(hasher.hexdigest())
+
+    textbox = Label(root, text=(hasher.hexdigest()), font=('arial', 10), bd=4, relief=SUNKEN, anchor=CENTER)
+    textbox.pack(side=TOP, fill=X)
+
+    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+    if f is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    text2save = str(hasher.hexdigest())
+    f.write(text2save)
+    f.close()
+
+
 root = tk.Tk()
+top = Frame(root)
+top.pack(side=TOP)
 
+button1 = Button(root, text="Get SHA-256", fg="green", command=open_file)
+button1.config()
+button1.pack(in_=top, side=LEFT)
 
-button = Button(root, text="Get SHA-256", fg="green", command=open_file)
-button.pack()
+button2 = Button(root, text="Get SHA-512", fg="green", command=get_file)
+button2.config()
+button2.pack(in_=top, side=LEFT)
 
-button = Button(root, text="Get SHA-512", fg="green", command=get_file)
-button.pack()
+button3 = Button(root, text="Get MD5 Hash", fg="green", command=fetch_file)
+button3.config()
+button3.pack(in_=top, side=LEFT)
 
-Label(root, text="Checksum SHA-256 & SHA-512", fg="blue", bg="#dedcdc").pack()
+Label(root, text="Checksum SHA-256, SHA-512, & MD5", fg="blue", bg="#dedcdc").pack()
 root.title('Checksum Hash Extractor')
 root.configure(background='#dedcdc')
 root.geometry("900x180")
